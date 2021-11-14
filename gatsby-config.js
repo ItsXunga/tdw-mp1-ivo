f5f435a8ec34449538e59dd9eeff524bb9a30d02
Xunga <36819968+ItsXunga@users.noreply.github.com>
2021-11-14 18:40:59 +0000

    work plz

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-});
+})
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken:
     process.env.CONTENTFUL_ACCESS_TOKEN ||
     process.env.CONTENTFUL_DELIVERY_TOKEN,
-};
+}
 
-const { spaceId, accessToken } = contentfulConfig;
+const { spaceId, accessToken } = contentfulConfig
 
 if (!spaceId || !accessToken) {
   throw new Error(
     "Contentful spaceId and the access token need to be provided."
-  );
+  )
 }
 
 module.exports = {
@@ -34,4 +34,4 @@ module.exports = {
       options: contentfulConfig,
     },
   ],
-};
+}
--- src/components/tags.js
+++ src/components/tags.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react"
 
-import * as styles from './tags.module.css'
+import * as styles from "./tags.module.css"
 
 const Tags = ({ tags }) =>
   tags?.length > 0 && (
     <small className={styles.tags}>
-      {tags.map((tag) => (
+      {tags.map(tag => (
         <div key={tag} className={styles.tag}>
-
           {tag}
         </div>
       ))}
